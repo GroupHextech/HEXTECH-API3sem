2c624ab6ce51ae9eb6c97dda92b3ca3409ddd161
williamantoniazzi <william.antoniazzi@fatec.sp.gov.br>
2022-10-09 12:51:52 -0300

    Atualização documentação BD e Fix SpringBoot

--- ioweather/src/main/java/com/iacit/ioweather/model/Estacao.java
+++ ioweather/src/main/java/com/iacit/ioweather/model/Estacao.java
@@ -18,24 +18,25 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Estacao {
+
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private Long idEstacaibr;
+	private Long idEstacao;
 	
 	@NotEmpty(message="O nome da estaçao nao deve ser nulo")
 	@Size(max=60, message="Número máximo de caracteres é de 60.")
 	private String nomeEstacao;
 	
 	@NotEmpty(message="Latitude não pode ser vazio.")
-	private String latitudeEstacao;
+	private String latitude;
 	
 	@NotEmpty(message="Longitude não pode ser vazio.")
-	private String longitudeEstacao;
+	private String longitude;
 	
 	@NotNull(message="A altitude nao deve ser nula")
 	@DecimalMin(value="0.01", message=" A altitude da Estaçao nao pode ser menor que 0,01")
-	private BigDecimal altitudeEstacao;
+	private BigDecimal altitude;
 	
 	@NotNull(message="A data de fundaçao nao deve ser nula")
 	@DateTimeFormat(pattern="yyyy-MM-dd")
@@ -43,14 +44,14 @@ public class Estacao {
 	private Date dataFundacao;
 	
 	@NotEmpty(message="O id do estado nao pode estar vazio")
-	private String idEstado;
+	private String idEstadoEstacao;
 	
 	//Getters and Setters
-	public Long getIdEstacaibr() {
-		return idEstacaibr;
+	public Long getIdEstacao() {
+		return idEstacao;
 	}
-	public void setIdEstacaibr(Long idEstacaibr) {
-		this.idEstacaibr = idEstacaibr;
+	public void setIdEstacao(Long idEstacao) {
+		this.idEstacao = idEstacao;
 	}
 	public String getNomeEstacao() {
 		return nomeEstacao;
@@ -58,23 +59,23 @@ public class Estacao {
 	public void setNomeEstacao(String nomeEstacao) {
 		this.nomeEstacao = nomeEstacao;
 	}
-	public String getLatitudeEstacao() {
-		return latitudeEstacao;
+	public String getLatitude() {
+		return latitude;
 	}
-	public void setLatitudeEstacao(String latitudeEstacao) {
-		this.latitudeEstacao = latitudeEstacao;
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
 	}
-	public String getLongitudeEstacao() {
-		return longitudeEstacao;
+	public String getLongitude() {
+		return longitude;
 	}
-	public void setLongitudeEstacao(String longitudeEstacao) {
-		this.longitudeEstacao = longitudeEstacao;
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
 	}
-	public BigDecimal getAltitudeEstacao() {
-		return altitudeEstacao;
+	public BigDecimal getAltitudeo() {
+		return altitude;
 	}
-	public void setAltitudeEstacao(BigDecimal altitudeEstacao) {
-		this.altitudeEstacao = altitudeEstacao;
+	public void setAltitude(BigDecimal altitude) {
+		this.altitude = altitude;
 	}
 	public Date getDataFundacao() {
 		return dataFundacao;
@@ -82,15 +83,14 @@ public class Estacao {
 	public void setDataFundacao(Date dataFundacao) {
 		this.dataFundacao = dataFundacao;
 	}
-	public String getIdEstado() {
-		return idEstado;
+	public String getIdEstadoEstacao() {
+		return idEstadoEstacao;
 	}
-	public void setIdEstado(String idEstado) {
-		this.idEstado = idEstado;
+	public void setIdEstadoEstacao(String idEstadoEstacao) {
+		this.idEstadoEstacao = idEstadoEstacao;
 	}
 	
-	
-	//Metodos
+
 	
 
 }
