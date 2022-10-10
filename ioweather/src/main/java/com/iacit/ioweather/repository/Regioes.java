9ddf8bf9460f5777d4b7b54e2ebfb65625991cdd
williamantoniazzi <william.antoniazzi@fatec.sp.gov.br>
2022-10-09 23:01:22 -0300

    Criação dos controller Regiao e Estado

--- ioweather/src/main/java/com/iacit/ioweather/repository/Regioes.java
+++ ioweather/src/main/java/com/iacit/ioweather/repository/Regioes.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.iacit.ioweather.model.Regiao;
 
-public interface Regioes extends JpaRepository<Regiao,Long>{
+public interface Regioes extends JpaRepository<Regiao, Long>{
 
 }
